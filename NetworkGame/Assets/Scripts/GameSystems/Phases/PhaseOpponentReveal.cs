5526a99389ee72c82e0d1555054fec9150941ff1
augustlewen <augustlewen@live.com>
2024-10-03 14:52:57 +0200

    added phase for opponent revela

---
+++ NetworkGame/Assets/Scripts/GameSystems/Phases/PhaseOpponentReveal.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using GameSystems.Battle;
+using GameSystems.Player;
+using TMPro;
+using UnityEngine;
+
+namespace GameSystems.Phases
+{
+    public class PhaseOpponentReveal : BasePhase
+    {
+        public Transform background;
+        public TextMeshProUGUI guildText;
+        public TextMeshProUGUI vsText;
+        public TextMeshProUGUI opponentText;
+        
+        private void Start()
+        {
+            PlayerStats.i.onPlayerSetupComplete.AddListener(OnPlayerSetupComplete);
+            BattleRoomManager.i.OnOpponentsPrepared.AddListener(OnOpponentsPrepared);
+        }
+
+        private void OnPlayerSetupComplete()
+        {
+            guildText.text = PlayerStats.GetGuildStats().guildName;
+            guildText.color = PlayerStats.GetGuildStats().guildColor;
+        }
+
+        public override void OnBeginPhase()
+        {
+            base.OnBeginPhase();
+            
+            background.gameObject.SetActive(true);
+            BattleRoomManager.i.PrepareBattleOpponents();
+        }
+
+        private void OnOpponentsPrepared(List<BattleRoomManager.BattleRoom> arg0)
+        {
+            StartCoroutine(OpponentRevealAnimation());
+        }
+
+        public override void OnEndPhase()
+        {
+            base.OnEndPhase();
+            
+            guildText.gameObject.SetActive(false);
+            vsText.gameObject.SetActive(false);
+            opponentText.gameObject.SetActive(false);
+            background.gameObject.SetActive(false);
+        }
+
+        IEnumerator OpponentRevealAnimation()
+        {
+            yield return new WaitForSeconds(1);
+
+            guildText.gameObject.SetActive(true);
+            guildText.GetComponent<Animator>().Play("TextComingAtScreen", 0, 0);
+            yield return new WaitForSeconds(1);
+            
+            vsText.gameObject.SetActive(true);
+            vsText.GetComponent<Animator>().Play("TextComingAtScreen", 0, 0);
+            yield return new WaitForSeconds(1);
+
+            opponentText.text = BattleManager.i.opponent == null ? "Monster" : BattleManager.i.opponent.guildName;
+            opponentText.color = BattleManager.i.opponent == null ? Color.grey : BattleManager.i.opponent.guildColor;
+                
+            opponentText.gameObject.SetActive(true);
+            opponentText.GetComponent<Animator>().Play("TextComingAtScreen", 0, 0);
+        }
+        
+    }
+}
