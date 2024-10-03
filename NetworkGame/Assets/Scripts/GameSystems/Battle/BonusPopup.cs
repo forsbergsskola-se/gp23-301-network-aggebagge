e4142ade6ec9a293602890c34eb8c6625f7538bf
augustlewen <augustlewen@live.com>
2024-10-03 20:03:31 +0200

    updated bonuspopups

--- NetworkGame/Assets/Scripts/GameSystems/Battle/BonusPopup.cs
+++ NetworkGame/Assets/Scripts/GameSystems/Battle/BonusPopup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameSystems.Battle
 {
@@ -11,37 +13,52 @@ namespace GameSystems.Battle
         private float startYPos;
 
         private RectTransform rectTransform;
+        public TextMeshProUGUI text;
+        public Image image;
 
         private void Awake()
         {
             startYPos = transform.position.y;
-            StartCoroutine(DestroyDelay());
+            // StartCoroutine(DestroyDelay());
             
             rectTransform = GetComponent<RectTransform>();
 
         }
 
-        private void Update()
+        public void SetText(bool isDamage, int value)
         {
-            // var position = transform.position;
-            // position = new Vector3(position.x, Mathf.Lerp(position.y, startYPos + targetYPos, Time.deltaTime * speed));
-            // transform.position = position;
-            
-
-            // Get the current anchored position
-            Vector2 position = rectTransform.anchoredPosition;
-
-            // Apply the same vertical interpolation using Mathf.Lerp, but to the y component of anchoredPosition
-            position = new Vector2(position.x, Mathf.Lerp(position.y, startYPos + targetYPos, Time.deltaTime * speed));
-
-            // Set the updated position back to the RectTransform
-            rectTransform.anchoredPosition = position;
+            text.text = value.ToString();
+            text.color = isDamage ? GameManager.i.damageColor : GameManager.i.goldColor;
+            text.gameObject.SetActive(true);
         }
         
-        IEnumerator DestroyDelay()
+        public void SetIcon(Sprite sprite)
         {
-            yield return new WaitForSeconds(1.5f);
-            Destroy(gameObject);
+            image.sprite = sprite;
+            image.gameObject.SetActive(true);
         }
+
+        // private void Update()
+        // {
+        //     // var position = transform.position;
+        //     // position = new Vector3(position.x, Mathf.Lerp(position.y, startYPos + targetYPos, Time.deltaTime * speed));
+        //     // transform.position = position;
+        //     
+        //
+        //     // Get the current anchored position
+        //     Vector2 position = rectTransform.anchoredPosition;
+        //
+        //     // Apply the same vertical interpolation using Mathf.Lerp, but to the y component of anchoredPosition
+        //     position = new Vector2(position.x, Mathf.Lerp(position.y, startYPos + targetYPos, Time.deltaTime * speed));
+        //
+        //     // Set the updated position back to the RectTransform
+        //     rectTransform.anchoredPosition = position;
+        // }
+        
+        // IEnumerator DestroyDelay()
+        // {
+        //     yield return new WaitForSeconds(1.5f);
+        //     Destroy(gameObject);
+        // }
     }
 }
