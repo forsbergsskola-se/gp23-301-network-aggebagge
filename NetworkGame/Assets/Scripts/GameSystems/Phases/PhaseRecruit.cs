fb7648d098450eb4d993755221058c59ef64d712
augustlewen <augustlewen@live.com>
2024-10-03 12:33:59 +0200

    added some logic in phase recruit

--- NetworkGame/Assets/Scripts/GameSystems/Phases/PhaseRecruit.cs
+++ NetworkGame/Assets/Scripts/GameSystems/Phases/PhaseRecruit.cs
@@ -1,7 +1,21 @@
+using UnityEngine;
+
 namespace GameSystems.Phases
 {
     public class PhaseRecruit : BasePhase
     {
-        
+        public Transform recruitTransform;
+
+        public override void OnBeginPhase()
+        {
+            base.OnBeginPhase();
+            recruitTransform.gameObject.SetActive(true);
+        }
+
+        public override void OnEndPhase()
+        {
+            base.OnEndPhase();
+            recruitTransform.gameObject.SetActive(false);
+        }
     }
 }
